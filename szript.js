d11df3347ff1bffff14b5c949711810c7670d2a5
Chandiniabdul <137866739+Chandiniabdul@users.noreply.github.com>
2025-08-10 16:46:52 +0530

    szript.js

--- szript.js
+++ szript.js
@@ -1,1 +1,16 @@
+function swithOff() {
+    document.getElementById("bulbimage").src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+    document.getElementById("catimage").src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+    document.getElementById("switchStatus").textContent = "Switched Off";
+    document.getElementById("Offswitch").style.backgroundColor = "#cbd2d9";
+    document.getElementById("Onswitch").style.backgroundColor = "#22c55e";
+}
 
+function swithOn() {
+    document.getElementById("bulbimage").src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+    document.getElementById("catimage").src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
+    document.getElementById("Offswitch").style.backgroundColor = "#e12d39";
+    document.getElementById("Onswitch").style.backgroundColor = "#cbd2d9";
+
+
+}
